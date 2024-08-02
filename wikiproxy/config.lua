2e7bce6a6c130b0c0a1ffaad095ab9bc6cb58afd
Aaron LI <aly@aaronly.me>
2024-08-02 13:37:37 +0800

    wikiproxy: Implement simple auth function to protect abuses

--- wikiproxy/config.lua
+++ wikiproxy/config.lua
@@ -7,6 +7,18 @@
 local ngx = ngx
 
 local _M = {
+    -- Configure the simple authentication function.
+    -- (implemented to protect from crawlers and public accesses/abuses)
+    auth = {
+        -- Status code to reply if not yet authenticated.
+        code = ngx.HTTP_NOT_FOUND,
+        -- Number of retries to pass the authentication.
+        retries = 6,
+        -- Time to wait for the client to finish the authentication.
+        wait_time = 10, -- [seconds]
+        -- Validity time of a successful authentication.
+        ttl = 3600, -- [seconds]
+    },
 }
 
 
--- wikiproxy/request.lua
+++ wikiproxy/request.lua
@@ -6,11 +6,35 @@
 
 local ngx = ngx
 
+local xconfig = require("wikiproxy.config")
+
+local xshdict = ngx.shared["wikiproxy"]
+
 local _M = {}
 
 
 function _M.handle()
-    -- TODO
+    -- Simple authentication to prevent crawlers and public accesses/abuses.
+    --
+    local client_ip = ngx.var.remote_addr
+    local user_agent = ngx.var.http_user_agent
+
+    local key_authed = "authed:" .. client_ip .. ":" .. user_agent
+    if xshdict:get(key_authed) then
+        return
+    end
+
+    local key_authing = "authing:" .. client_ip .. ":" .. user_agent
+    local v = xshdict:incr(key_authing, 1, 0, xconfig.auth.wait_time)
+    if v and v <= xconfig.auth.retries then
+        ngx.status = xconfig.auth.code
+        ngx.say(tostring(xconfig.auth.retries + 1 - v))
+        return
+    end
+
+    xshdict:set(key_authed, "dummy", xconfig.auth.ttl)
+    ngx.log(ngx.INFO, "authenticated: ip=[", client_ip, "], user_agent=[",
+            user_agent, "]")
 end
 
 
