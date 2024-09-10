022f27703fd5a69912aabf8f087732289a804cbb
Aaron LI <aly@aaronly.me>
2024-09-10 09:51:57 +0800

    wikiproxy: Deduplicate some code in auth.lua

--- wikiproxy/auth.lua
+++ wikiproxy/auth.lua
@@ -17,18 +17,19 @@ local xshdict = ngx.shared["wikiproxy"]
 
 local _M = {}
 
+local function exit(code, content)
+    ngx.status = code
+    ngx.header["Content-Type"] = "text/plain"
+    ngx.header["Content-Length"] = #content + 1
+    ngx.say(content)
+end
 
 function _M.handle()
     -- Simple authentication to prevent crawlers and public accesses/abuses.
     --
     local user_agent = ngx.var.http_user_agent
     if not user_agent or user_agent == "" then
-        local content = "bad request"
-        ngx.status = 400
-        ngx.header["Content-Type"] = "text/plain"
-        ngx.header["Content-Length"] = #content + 1
-        ngx.say(content)
-        return
+        return exit(400, "bad request")
     end
 
     local client_ip = ngx.var.remote_addr
@@ -41,11 +42,7 @@ function _M.handle()
     local v = xshdict:incr(key_authing, 1, 0, xconfig.auth.wait_time)
     if v and v <= xconfig.auth.retries then
         local content = tostring(xconfig.auth.retries + 1 - v)
-        ngx.status = xconfig.auth.code
-        ngx.header["Content-Type"] = "text/plain"
-        ngx.header["Content-Length"] = #content + 1
-        ngx.say(content)
-        return
+        return exit(xconfig.auth.code, content)
     end
 
     xshdict:set(key_authed, "dummy", xconfig.auth.ttl)
